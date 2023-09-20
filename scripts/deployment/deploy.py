f1917f0daf7a2d7bf932ccfbe41423335a6e35ea
David Benedeki <14905969+benedeki@users.noreply.github.com>
2023-09-20 19:32:50 +0200

    Apply suggestions from code review
    Co-authored-by: Ladislav Sulak <laco.sulak@gmail.com>
    Co-authored-by: miroslavpojer <miroslav.pojer@absa.africa>

--- scripts/deployment/deploy.py
+++ scripts/deployment/deploy.py
@@ -65,7 +65,7 @@ def parse_args() -> argparse.Namespace:
     return parser.parse_args()
 
 
-def execute_sql(conn_config: PostgresDBConn, sql: str):
+def execute_sql(conn_config: PostgresDBConn, sql: str) -> None:
     conn = psycopg2.connect(
         database=conn_config.database,
         user=conn_config.username,
@@ -98,7 +98,7 @@ def ensure_trailing_slash(path: str) -> str:
         return path + '/'
 
 
-def process_dir(directory: str, conn_config: PostgresDBConn, create_db: bool):
+def process_dir(directory: str, conn_config: PostgresDBConn, create_db: bool) -> None:
     logging.info(f"Picking up source files from directory `{directory}`")
     public_schema = "public"
     root = next(os.walk(directory), (None, [], []))
@@ -110,9 +110,9 @@ def process_dir(directory: str, conn_config: PostgresDBConn, create_db: bool):
     init_sqls = []
     for filename in files:
         if filename.startswith("00_"):
-            database_creation_sqls.append(filename)
+            database_creation_sqls.append(os.path.join(directory, filename))
         else:
-            init_sqls.append(filename)
+            init_sqls.append(os.path.join(directory, filename))
     # process schemas
     schemas_sqls = []
     if public_schema in schemas:
@@ -141,7 +141,7 @@ def process_dir(directory: str, conn_config: PostgresDBConn, create_db: bool):
         execute_sql(conn_config, sql)
 
 
-def process_schema(base_dir: str,  schema_name: str, expect_schema_creation: bool) -> list[str]:
+def process_schema(base_dir: str, schema_name: str, expect_schema_creation: bool) -> list[str]:
     logging.info(f"  - schema '{schema_name}'")
     schema_dir = ensure_trailing_slash(base_dir + schema_name)
     schema_creation = []
@@ -152,14 +152,14 @@ def process_schema(base_dir: str,  schema_name: str, expect_schema_creation: boo
     files.sort()
     for input_file in files:
         if input_file == "_.ddl":
-            schema_creation = [read_file(schema_dir + input_file)]
+            schema_creation = [read_file(os.path.join(schema_dir, input_file))]
             has_schema_creation = True
         elif input_file.endswith(".sql"):
             functions.append(read_file(schema_dir + input_file))
         elif input_file.endswith(".ddl"):
             tables.append(read_file(schema_dir + input_file))
 
-    if expect_schema_creation and (not has_schema_creation):
+    if expect_schema_creation and not has_schema_creation:
         logging.warning(f"No schema creation found on path `{schema_dir}`")
 
     return schema_creation + functions + tables
