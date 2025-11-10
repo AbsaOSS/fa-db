929243ae43f390716aeb9f806f03ded518c4648d
miroslavpojer <miroslav.pojer@absa.africa>
2025-11-10 15:30:10 +0100

    Experiment with smaller update.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -45,10 +45,10 @@ object Dependencies {
 
   def doobieDependencies(scalaVersion: String): Seq[ModuleID] = {
     commonDependencies(scalaVersion) ++ Seq(
-      "org.tpolecat" %% "doobie-core" % "1.0.0-RC11",
-      "org.tpolecat" %% "doobie-hikari" % "1.0.0-RC11",
-      "org.tpolecat" %% "doobie-postgres" % "1.0.0-RC11",
-      "org.tpolecat" %% "doobie-postgres-circe" % "1.0.0-RC11",
+      "org.tpolecat" %% "doobie-core" % "1.0.0-RC3",
+      "org.tpolecat" %% "doobie-hikari" % "1.0.0-RC3",
+      "org.tpolecat" %% "doobie-postgres" % "1.0.0-RC3",
+      "org.tpolecat" %% "doobie-postgres-circe" % "1.0.0-RC3",
       "io.circe" %% "circe-generic" % "0.14.15" % Test
     )
   }
